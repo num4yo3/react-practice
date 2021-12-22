a05134683980ecf76184b7beeac25c55b3070b91
num4yo3 <82578883+num4yo3@users.noreply.github.com>
2021-12-22 18:52:15 +0900

    コメントボックスコンポーネント化始めるよ

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
+import { useState } from "react";
 import "./styles.css";
 import { Menu } from "./components/Menu";
 import { DoubleButton } from "./components/DoubleButton";
 
 export const App = () => {
+  const [text, setText] = useState("");
+  const [list, setList] = useState([]);
+  const onChangeTextbox = (event) => setText(event.target.value);
+  const onClickButton = () => {
+    setText("");
+    setList([...list, text]);
+  };
   return (
     <>
       <h1>Hello CodeSandbox</h1>
       <p>highlight the text with mouseover.</p>
-      <h2>show text with mouseover on button</h2>
+      <h2>expand menu with mouseover on button</h2>
       <Menu>Menu00</Menu>
 
       <h2>show input text</h2>
       <p>double input data.</p>
       <DoubleButton />
+
+      <h2>log of input text</h2>
+      <input type="text" value={text} onChange={onChangeTextbox} />
+      <button type="button" onClick={onClickButton}>
+        記録
+      </button>
+      <ul>
+        {list.map((value, index) => {
+          return <li key={value}>{index + ": " + value}</li>;
+        })}
+      </ul>
     </>
   );
 };
